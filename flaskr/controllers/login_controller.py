5c71baa66bcead1d4a2cfa98451411183bd5f526
JaiderYesid <jaiderjr084@gmail.com>
2025-08-14 11:49:58 -0500

    Arreglo importacion

--- flaskr/controllers/login_controller.py
+++ flaskr/controllers/login_controller.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
 from flaskr.modelos import Usuario
-from flaskr.modelos import db
+from flaskr import db
 import datetime
 
 login_bp = Blueprint('login', __name__)
